4dd53d87f68e8ce64b1bdd428dffbed91192a402
Ross Bickmore <rossbickmore@gmail.com>
2019-09-09 15:24:59 +0100

    created random number function and linked to button

--- src/index.js
+++ src/index.js
@@ -1,12 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const App = () => {
+  
+  const [selected, setSelected] = useState(0)
+
+  const randomNumber = () => Math.round(Math.random()*4)
+
+  const quotes = [ 
+    {"quoteText": "Genius is one percent inspiration and ninety-nine percent perspiration.",
+    "quoteAuthor": "Thomas Edison"
+    }, {
+    "quoteText": "You can observe a lot just by watching.",
+    "quoteAuthor": "Yogi Berra"
+    }, {
+    "quoteText": "A house divided against itself cannot stand.",
+    "quoteAuthor": "Abraham Lincoln"
+    }, {
+    "quoteText": "Difficulties increase the nearer we get to the goal.",
+    "quoteAuthor": "Johann Wolfgang von Goethe"
+    }, {
+    "quoteText": "Fate is in your hands and no one elses",
+    "quoteAuthor": "Byron Pulsifer"
+    }
+  ]
+  
+return (
+  <div>
+    <h1>{quotes[selected].quoteText}</h1>
+    <p>{quotes[selected].quoteAuthor}</p>
+    <button onClick={() => setSelected(randomNumber())}>New quote</button>
+  </div>
+    
+
+)
+  
+  
+}
+
+ReactDOM.render(<App />, 
+  document.getElementById('root')
+)
+
